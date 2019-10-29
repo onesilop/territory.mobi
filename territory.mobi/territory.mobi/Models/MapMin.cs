ee2421ff79e5fa72abdf322b1dac8bde8dddac5c
Carlo <cpoliseno@agl.com.au>
2019-10-30 08:58:55 +1100

    Updates for API access.

---
+++ territory.mobi/territory.mobi/Models/MapMin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace territory.mobi.Models
+{
+    public partial class MapMin
+    {
+
+        
+        public MapMin()
+        {
+            DoNotCalls = new HashSet<DoNotCall>();
+        }
+
+        public string MapKey { get; set; }
+        public string MapDesc { get; set; }
+        public string MapArea { get; set; }
+        public string Notes { get; set; }
+        public string Parking { get; set; }
+        public string Section { get; set; }
+        public int SortOrder { get; set; }
+        public Boolean Display { get; set; }
+        public DateTime UpdateDatetime { get; set; }
+        public byte[] ImgImage { get; set; }
+
+        public virtual ICollection<DoNotCall> DoNotCalls { get; set; }
+    }
+}
